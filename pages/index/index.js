b6cb644b457746ebae9c7a59cdb0400f03f2e6e6
ArcticFoxPro <139336664+ArcticFoxPro@users.noreply.github.com>
2024-03-17 13:34:15 +0800

    使用TDesign Popup重写关于界面，刷新支持显示加载动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,66 +2,88 @@
 const QQVersionBean = require('./QQVersionBean.js');
 
 Page({
-  options: {
-    styleIsolation: 'apply-shared',
-  },
-  data: {
-    qqVersions: [],
-    aboutBaseBtn: { content: '确定', variant: 'base' },
-    dialogKey: '',
-    showTooLongBtnContent: false,
-  },
-  onLoad: function () {
-    this.getData();
-  },
-  refreshData: function(){
-    this.getData();
-  },
-  showDialog(e) {
-    const { key } = e.currentTarget.dataset;
-    this.setData({ [key]: true, dialogKey: key });
-  },
-  closeDialog() {
-    const { dialogKey } = this.data;
-    this.setData({ [dialogKey]: false });
-  },
-  getData: function () {
-    wx.request({
-      url: 'https://im.qq.com/rainbow/androidQQVersionList',
-      method: 'GET',
-      success: (res) => {
-        try {
-          let responseData = res.data;
-          let start = responseData.indexOf("versions64\":[") + 12;
-          let end = responseData.indexOf(";\n" + "      typeof");
-          let totalJson = responseData.substring(start, end);
-          let qqVersionJsons = totalJson.split("},{").reverse();
+    options: {
+        styleIsolation: 'apply-shared',
+    }, data: {
+        qqVersions: [], onRefresh: false, refreshIcon: "refresh",
+    }, onLoad: function () {
+        this.getData();
+    }, refreshData: function () {
+        this.getData();
+    }, copyOSLink() {
+        wx.setClipboardData({
+            data: 'https://github.com/klxiaoniu/QQVersionList', success(res) {
+            }, fail: function (res) {
+            }
+        })
+    }, aboutPopupVisible(e) {
+        this.setData({
+            aboutVisible: e.detail.visible,
+        });
+    }, handleAboutPopup(e) {
+        const {item} = e.currentTarget.dataset;
+        this.setData({
+            cur: item,
+        }, () => {
+            this.setData({aboutVisible: true});
+        },);
+    }, closeAboutPopup(e) {
+        const {item} = e.currentTarget.dataset;
+        this.setData({
+            cur: item,
+        }, () => {
+            this.setData({aboutVisible: false});
+        },);
+    }, getData: function () {
+        this.setData({refreshIcon: null,});
+        this.setData({
+            onRefresh: true,
+        });
+        wx.request({
+            url: 'https://im.qq.com/rainbow/androidQQVersionList', method: 'GET', success: (res) => {
+                try {
+                    let responseData = res.data;
+                    let start = responseData.indexOf("versions64\":[") + 12;
+                    let end = responseData.indexOf(";\n" + "      typeof");
+                    let totalJson = responseData.substring(start, end);
+                    let qqVersionJsons = totalJson.split("},{").reverse();
 
-          let qqVersionList = [];
-          for (let jsonStr of qqVersionJsons) {
-            let pstart = jsonStr.indexOf("{\"versions");
-            let pend = jsonStr.indexOf(",\"length");
-            let json = jsonStr.substring(pstart, pend);
+                    let qqVersionList = [];
+                    for (let jsonStr of qqVersionJsons) {
+                        let pstart = jsonStr.indexOf("{\"versions");
+                        let pend = jsonStr.indexOf(",\"length");
+                        let json = jsonStr.substring(pstart, pend);
 
-            let qqVersionBean = JSON.parse(json);
-            // 假设QQVersionBean的结构与Kotlin中的QQVersionBean一致
+                        let qqVersionBean = JSON.parse(json);
+                        // 假设QQVersionBean的结构与Kotlin中的QQVersionBean一致
 
-            qqVersionList.push(qqVersionBean);
-          }
+                        qqVersionList.push(qqVersionBean);
+                    }
 
-          this.setData({
-            qqVersions: qqVersionList,
-          });
-        } catch (e) {
-          console.error(e);
-          // 在这里处理错误，如显示错误提示对话框等
-        }
-      },
-      fail: (err) => {
-        console.error(err);
-        // 在这里处理网络请求失败的情况
-      },
-    });
-  },
+                    this.setData({
+                        qqVersions: qqVersionList,
+                    });
+                    this.setData({
+                        onRefresh: false,
+                    });
+                    this.setData({refreshIcon: "refresh",});
+                } catch (e) {
+                    this.setData({
+                        onRefresh: false,
+                    });
+                    this.setData({refreshIcon: "refresh",});
+                    console.error(e);
+                    // 在这里处理错误，如显示错误提示对话框等
+                }
+            }, fail: (err) => {
+                this.setData({
+                    onRefresh: false,
+                });
+                this.setData({refreshIcon: "refresh",});
+                console.error(err);
+                // 在这里处理网络请求失败的情况
+            },
+        });
+    },
 
 })
