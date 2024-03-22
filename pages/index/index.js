087ae52c83703507410b34477ba3225889f14be6
ArcticFoxPro <139336664+ArcticFoxPro@users.noreply.github.com>
2024-03-22 15:07:38 +0800

    修复滚动列表和底部按钮栏重叠的问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,40 +5,65 @@ Page({
     options: {
         styleIsolation: 'apply-shared',
     }, data: {
-        qqVersions: [], onRefresh: false, refreshIcon: "refresh",
+        qqVersions: [], onRefresh: false, refreshIcon: "refresh", versionSmallVisible: wx.getStorage({
+            key: "versionSelect", success(res) {
+                console.log(res.data)
+            }
+        }), seeJson: "查看 Json 字符串", titleTop: "", heightRecycle: 5000,
     }, onLoad: function () {
         this.getData();
-    }, refreshData: function () {
+    }, onReady: async function () {
+        const windowHeight = await new Promise((resolve) => {
+            wx.getSystemInfo({
+                success: (res) => {
+                    resolve(res.windowHeight);
+                },
+            });
+        });
+
+        let elementHeight1, elementHeight2, elementHeight3;
+
+        // 获取元素高度的函数封装
+        const getElementHeight = async (selector) => {
+            return new Promise((resolve) => {
+                const query = wx.createSelectorQuery();
+                query.select(selector).boundingClientRect();
+
+                query.exec((res) => {
+                    if (res[0]) {
+                        resolve(res[0].height);
+                    } else {
+                        console.log('未找到对应的元素：', selector);
+                        resolve(0); // 如果找不到元素，返回默认值0
+                    }
+                });
+            });
+        };
+
+        // 获取各个元素高度
+        elementHeight1 = await getElementHeight('#titleTop1');
+        elementHeight2 = await getElementHeight('#titleTop2');
+        elementHeight3 = await getElementHeight('#bottomButton');
+
+        console.log('窗口高度：', windowHeight);
+        console.log('元素高度1：', elementHeight1);
+        console.log('元素高度2：', elementHeight2);
+        console.log('元素高度3：', elementHeight3);
+
+        this.setData({heightRecycle: windowHeight - elementHeight1 - elementHeight2 - elementHeight3});
+    },
+
+    refreshData: function () {
         this.getData();
     }, copyOSLink() {
         wx.setClipboardData({
-            data: 'https://github.com/klxiaoniu/QQVersionList', success(res) {
+            data: 'https://github.com/ArcticFoxPro/QQVersionListTool-WeChatMiniProgram', success(res) {
             }, fail: function (res) {
             }
         })
-    }, aboutPopupVisible(e) {
-        this.setData({
-            aboutVisible: e.detail.visible,
-        });
-    }, handleAboutPopup(e) {
-        const {item} = e.currentTarget.dataset;
-        this.setData({
-            cur: item,
-        }, () => {
-            this.setData({aboutVisible: true});
-        },);
-    }, closeAboutPopup(e) {
-        const {item} = e.currentTarget.dataset;
-        this.setData({
-            cur: item,
-        }, () => {
-            this.setData({aboutVisible: false});
-        },);
     }, getData: function () {
         this.setData({refreshIcon: null,});
-        this.setData({
-            onRefresh: true,
-        });
+        this.setData({onRefresh: true,});
         wx.request({
             url: 'https://im.qq.com/rainbow/androidQQVersionList', method: 'GET', success: (res) => {
                 try {
@@ -55,7 +80,6 @@ Page({
                         let json = jsonStr.substring(pstart, pend);
 
                         let qqVersionBean = JSON.parse(json);
-                        // 假设QQVersionBean的结构与Kotlin中的QQVersionBean一致
 
                         qqVersionList.push(qqVersionBean);
                     }
@@ -63,27 +87,83 @@ Page({
                     this.setData({
                         qqVersions: qqVersionList,
                     });
-                    this.setData({
-                        onRefresh: false,
-                    });
+                    this.setData({onRefresh: false,});
                     this.setData({refreshIcon: "refresh",});
+
+                    this.setData({versionBig: qqVersionList[0].versionNumber})
                 } catch (e) {
-                    this.setData({
-                        onRefresh: false,
-                    });
+                    this.setData({onRefresh: false,});
                     this.setData({refreshIcon: "refresh",});
                     console.error(e);
                     // 在这里处理错误，如显示错误提示对话框等
                 }
             }, fail: (err) => {
-                this.setData({
-                    onRefresh: false,
-                });
+                this.setData({onRefresh: false,});
                 this.setData({refreshIcon: "refresh",});
                 console.error(err);
                 // 在这里处理网络请求失败的情况
             },
         });
-    },
+    }, aboutPopupVisible(e) {
+        this.setData({
+            aboutVisible: e.detail.visible,
+        });
+    }, handleAboutPopup() {
+        this.setData({aboutVisible: true});
+    }, closeAboutPopup() {
+        this.setData({aboutVisible: false});
+    }, guessPopupVisible(e) {
+        this.setData({
+            guessVisible: e.detail.visible,
+        });
+    }, handleGuessPopup() {
+        this.setData({guessVisible: true});
+    }, closeGuessPopup() {
+        this.setData({guessVisible: false});
+    }, copyAndroidOSLink() {
+        this.setData({guessVisible: false});
+        wx.setClipboardData({
+            data: 'https://github.com/klxiaoniu/QQVersionList', success(res) {
+            }, fail: function (res) {
+            }
+        })
+    }, clickCell(e) {
+        //console.log(e.currentTarget.dataset.index);
+        const index = e.currentTarget.dataset.index;
+        //this.setData({selectCell:index})
+        this.setData({itemVersion: this.data.qqVersions[index].versionNumber});
+        this.setData({itemSize: this.data.qqVersions[index].size});
+        this.setData({itemFeatureTitle: this.data.qqVersions[index].featureTitle});
+        //console.log(this.data.qqVersions[index].summary);
+        this.setData({itemSummary: this.data.qqVersions[index].summary});
+        this.setData({itemString: JSON.stringify(this.data.qqVersions[index], null, 2)});
+        this.setData({cellDetailVisible: true});
+
+
+    }, closeCellDetailPopup() {
+        this.setData({cellDetailVisible: false});
+    }, displayJson() {
+        this.setData({cellDetailVisible: false});
+
+        this.setData({cellJsonDetailVisible: true});
+    }, closeCellJsonDetailPopup() {
+        this.setData({cellJsonDetailVisible: false});
+    }, cellDetialPopupVisible(e) {
+        this.setData({
+            cellDetailVisible: e.detail.visible,
+        });
+    }, cellJsonDetialPopupVisible(e) {
+        this.setData({
+            cellJsonDetailVisible: e.detail.visible,
+        });
+    }, titleChange(e) {
+        console.log(e.detail.scrollTop)
+        if (e.detail.scrollTop >= 50) {
+            this.setData({titleTop: "QQ 版本列表 Lite"});
+            console.log("Yes")
+        } else {
+            this.setData({titleTop: ""});
+        }
+    }
 
 })
